4ae0f2cc915230cfcc58656c80336b7b912d21c7
H43apo8 <H43apo8@yandex.ru>
2022-11-30 19:44:28 +0300

    Является ли квадратом 1и 2 пример 2

---
+++ Workshop002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число 1");
+string input0 = Console.Readline();
+
+Console.WriteLine("Введите число 2");
+string input1 = Console.Readline();
+
+int num0 = Convert.ToInt32(input0);
+int num1 = Convert.ToInt32(input1);
+
+if (num1*num1) == num0 )
+{
+    Console.WriteLine($"Число {num1} является квадратом числа {num0}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не является квадратом числа {num0}");
+}
