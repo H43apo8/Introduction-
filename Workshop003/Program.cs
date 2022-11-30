8aa9bbd73af72a8a3b1cf2395ba06000e5a2528c
H43apo8 <H43apo8@yandex.ru>
2022-11-30 20:12:58 +0300

    Добавили else в пример 3

--- Workshop003/Program.cs
+++ Workshop003/Program.cs
@@ -5,19 +5,23 @@ if (day == "1")
 {
     Console.WriteLine("Понедельник");
 }
-if (day == "2")
+else if (day == "2")
 {
     Console.WriteLine("Вторник");
 }
-if (day == "3")
+else if (day == "3")
 {
     Console.WriteLine("Среда");
 }
-if (day == "4")
+else if (day == "4")
 {
     Console.WriteLine("Четверг");
 }
-if (day == "5")
+else if (day == "5")
 {
     Console.WriteLine("Пятница");
+}
+else
+{
+    Console.WriteLine("ВВели неверное число");
 }
