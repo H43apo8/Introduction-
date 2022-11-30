5205fb96351a59e8e793e9e0fdcf0f2e726f0801
H43apo8 <H43apo8@yandex.ru>
2022-11-30 19:18:50 +0300

    Пример возведение в квадрат

--- Workshop001/Program.cs
+++ Workshop001/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
