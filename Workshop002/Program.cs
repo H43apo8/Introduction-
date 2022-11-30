0003a106e1cbeb87596d3fe84ca9f02b23203dc6
H43apo8 <H43apo8@yandex.ru>
2022-11-30 20:04:09 +0300

    Пример 3 день недели

--- Workshop002/Program.cs
+++ Workshop002/Program.cs
@@ -7,7 +7,7 @@ string input1 = Console.Readline();
 int num0 = Convert.ToInt32(input0);
 int num1 = Convert.ToInt32(input1);
 
-if (num1*num1) == num0 )
+if (( num1 * num1 ) == num0 )
 {
     Console.WriteLine($"Число {num1} является квадратом числа {num0}");
 }
---
+++ Workshop003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число:");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if (day == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if (day == "3")
+{
+    Console.WriteLine("Среда");
+}
+if (day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if (day == "5")
+{
+    Console.WriteLine("Пятница");
+}
