1b9dc51346353cd486396ba8dd0ab7e40c6d0330
H43apo8 <H43apo8@yandex.ru>
2022-12-01 08:24:02 +0300

    Добавили пример 5

---
+++ Workshop005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+string nstring = Console.ReadLine();
+int n = Convert.ToInt32(nstring);
+
+if ((n >= 100) && (n <=999))
+{
+    Console.WriteLine(n % 10);
+}
+else
+    Console.WriteLine("Неверное число");
+    
