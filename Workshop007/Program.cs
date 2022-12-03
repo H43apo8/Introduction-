b95522ba7b0091caceb55c170284d7d66ab6f4ad
H43apo8 <H43apo8@yandex.ru>
2022-12-03 13:34:48 +0300

    Прим. 7 случайное 3хзначное число и удаляет вторую цифру этого числа

---
+++ Workshop007/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа выводит случайное 3хзначное число и удаляет вторую цифру этого числа
+int GetRandomNumder()
+{
+    return new Random().Next(100, 1000); //1 ..9
+}
+
+void ShowRandomNumber(int number)
+{
+    Console.WriteLine($"Слцчайное число: {number}");
+    string str = number.ToString();
+    Console.WriteLine($">{str[0]}{str[2]}");
+}
+
+ShowRandomNumber(GetRandomNumder());
