850520c00426c433ae310ab54cf1abd4243ed511
H43apo8 <H43apo8@yandex.ru>
2022-12-03 14:30:04 +0300

    Прим.9 кратно ли оно и 7 и 23 одновременно

---
+++ Worlshop009/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход число
+// и проверяет, кратно ли оно и 7 и 23 одновременно
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else Console.WriteLine("Число не кратно 7 и 23");
