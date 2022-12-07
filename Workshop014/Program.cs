acef5baa7ca0aed025932827cd0235c2022929f8
H43apo8 <H43apo8@yandex.ru>
2022-12-07 20:23:28 +0300

    Прим.14 выдаёт таблицу квадратов

---
+++ Workshop014/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"{i * i}");
+}
