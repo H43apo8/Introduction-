207c6893625377a1ce9b97572d1d178a139741ca
H43apo8 <H43apo8@yandex.ru>
2022-12-07 19:33:41 +0300

    Прим.12 показывает диапазон по четверти

---
+++ Workshop012/Program.cs
@@ -0,0 +1,33 @@
+﻿// Программа которая по заданному номеру четверти показывает диапозон коорд.
+
+string myMethod(int quarter)
+{
+     switch (quarter)
+     {
+        case 1:
+            return "x=0 ... +8      y = 0 ... +8";
+            break;
+        case 2:
+            return "x=0 ... -8      y = 0 ... +8";
+            break;
+        case 3:
+            return "x=0 ... -8      y = 0 ... -8";
+            break;
+        case 4:
+            return "x=0 ... +8      y = 0 ... -8";
+            break;
+        default:
+            return "Нужно ввести число от 1 до 4 (четверть)";
+     }
+     
+}
+
+Console.WriteLine(myMethod(1));
+Console.WriteLine("");
+Console.WriteLine(myMethod(2));
+Console.WriteLine("");
+Console.WriteLine(myMethod(3));
+Console.WriteLine("");
+Console.WriteLine(myMethod(4));
+Console.WriteLine("");
+Console.WriteLine(myMethod(6));
