324d1f1fd6dd69c384d0f105be87a9582c40426b
H43apo8 <H43apo8@yandex.ru>
2022-12-07 20:02:44 +0300

    Прим.13 расстояние м/д точками

---
+++ Workshop013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает на вход координаты двух точек и находит расстояние м/д ними в 2D плоскости
+
+Console.WriteLine("Координаты х первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Координаты y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Координаты х второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Координаты y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance (int x1, int y1, int x2, int y2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+    return result;
+
+}
+
+Console.WriteLine($"Расстояние между точками = {Distance(x1, y1, x2, y2)} ");
