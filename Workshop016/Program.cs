18bca107dda94001417133e9eb09a8e7b64a7f1d
H43apo8 <H43apo8@yandex.ru>
2022-12-10 12:10:21 +0300

    Прим.16 Считает кол-во input number

---
+++ Workshop016/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа считает количество введённых чисел
+Console.Clear();
+//int GetNumber(int message)
+int CountOfDigits(int x)
+{
+    int cnt = 0;
+    while (x>0)
+    {
+        cnt += 1;
+        x /= 10;
+    }
+    return cnt;
+}
+Console.WriteLine(CountOfDigits(123456));
