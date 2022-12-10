a51f8f9a02b8f060ab2a463fee9b0091d9fd1d45
H43apo8 <H43apo8@yandex.ru>
2022-12-10 13:38:50 +0300

    Прим.18 "Random 0 и 1 Length 8"

---
+++ Workshop018/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа создаёт массив из 8 элементов (0 и 1) и  заполненный массив в случайном порядке
+
+Console.Clear(); 
+
+int[] numbers = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(0, 2);
+    Console.Write(numbers[i] + " ");
+}
