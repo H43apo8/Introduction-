394c471f3afbd1bc828c4b137dbb7a6aee2bd74e
H43apo8 <H43apo8@yandex.ru>
2022-12-17 23:51:14 +0300

    Прим.26 "Конвертирование 10го числа в 2ичное"

---
+++ Workshop026/Program.cs
@@ -0,0 +1,25 @@
+﻿// напишите программу которая будет преобразовывать 10чное число в 2ичное
+Console.Clear();
+
+string GetBinaryFromDecimal(int decimalNumber)
+{
+    int number = decimalNumber;
+    int ostatok = 0;
+    string result = String.Empty;
+
+    while (number >= 1)
+    {
+        ostatok = number % 2;
+        number /= 2;
+        if (ostatok != 0)
+        {
+            result += "1";
+        }
+        else
+        {
+            result += "0";
+        }
+    }
+    return new string (result.Reverse().ToArray());
+}
+Console.WriteLine(GetBinaryFromDecimal(11));
