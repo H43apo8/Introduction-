2a42a9befa2f4d76c7d61d82feb2f676f5e382ea
H43apo8 <H43apo8@yandex.ru>
2022-12-18 00:03:23 +0300

    Прим.27 "Числа фибоначи"

---
+++ Workshop027/Program.cs
@@ -0,0 +1,18 @@
+﻿// Не используюя рекурсию выведите первые N числа Фибоначи
+Console.Clear();
+
+int N = 5;
+int x1 = 0;
+int x2 = 1;
+int res = 0;
+string str = "0 1";
+
+for (int i = 2;  i <= N; i++)
+{
+    res = x2 + x1;
+    x1 = x2;
+    x2 = res;
+    str += res.ToString() + " ";
+}
+
+Console.WriteLine(str);
