e7e7b911aed273acb6d566ed795fe4706e59942f
H43apo8 <H43apo8@yandex.ru>
2022-12-18 00:12:49 +0300

    Прим.28 "Создание копии заданного массива"

---
+++ Workshop028/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу которая будет задавать копию заданного массива с помощью поэлементного копирования
+Console.Clear();
+
+int[] array1 = {1, 2, 3, 4, 5};
+int[] array2 = new int[array1.Length];
+
+for (int i = 0; i < array1.Length; i++)
+    array2[i] = array1[i];
+
+Console.WriteLine(string.Join(" ", array2));
